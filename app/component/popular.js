7b3cf06501b6171c92232c26fbef5108101b60af
happyahluwalia <happyahluwalia@gmail.com>
2017-07-01 16:51:52 -0400

    Able to fetch & display list of popular repos based on subject on the UI

--- app/component/popular.js
+++ app/component/popular.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import api from '../utils/api';
+
 function SelectLanguage(props){
     var languages = ['All', 'Javascript', 'Java', 'Swift', 'Ruby', 'CSS', 'Python' ];
     return (     
@@ -25,11 +27,51 @@ SelectLanguage.propTypes = {
     onSelect : PropTypes.func.isRequired
 }
 
+function RepoGrid(props){
+    return (
+        <ul className="repoContainer">
+            {
+              props.repos.map(function(repo, index){
+                    return (
+                        <div className="repo" key={repo.id}>
+                        <li>
+                            <div className="repoRank">
+                                #{index + 1} Rank
+                            </div>
+
+                            <ul className="repoInfo">
+                                <li >
+                                    <img className="repoImage" 
+                                         src={repo.owner.avatar_url} 
+                                         alt="Repo owners image"/>
+                                </li>
+                                <li className="repoName">
+                                    <a href={repo.owner.html_url} >{repo.name}</a>
+                                </li>
+                                <li className="repoName">
+                                    @{repo.owner.login}
+                                </li>
+                                <li className="repoName">
+                                    {repo.stargazers_count} stars
+                                </li> 
+                            </ul> 
+                        </li >
+                        </div>
+                )
+            })}
+        </ul>
+    )
+}
+RepoGrid.propTypes = {
+    repos : PropTypes.array.isRequired
+}
+
 class Popular extends Component {
     constructor(props){
         super(props);
         this.state = {
-            selectedLanguage  : 'All'
+            selectedLanguage  : 'All',
+            repos: null
         }
 
        this.setLanguage = this.setLanguage.bind(this); 
@@ -38,15 +80,35 @@ class Popular extends Component {
     setLanguage(lang) {
         this.setState(function() {
             return {
-                selectedLanguage: lang
+                selectedLanguage: lang,
+                repos: null
             }
         })
-    }
-    render() {
         
+        api.fetchPopularRepos(lang)
+            .then(function(response){
+                this.setState(function() {
+                    return {
+                        repos:response
+                    }
+                })
+            }.bind(this)); 
+    }
+
+    componentDidMount(){
+        this.setLanguage(this.state.selectedLanguage);
+    }
+
+    render() {
         return (
             <div>
-                <SelectLanguage selectedLanguage={this.state.selectedLanguage} onSelect={this.setLanguage} />
+                <SelectLanguage 
+                        selectedLanguage={this.state.selectedLanguage} 
+                        onSelect={this.setLanguage} 
+                />
+                {!this.state.repos 
+                ? <p>LOADING</p>
+                : <RepoGrid repos={ this.state.repos } />}
             </div>
         );
     }
